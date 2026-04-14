4d0610aa879e17decc5be61c5c28ffe6e08c1ea4
suhail <suhailalikka110@gmail.com>
2026-04-14 10:24:25 +0530

    first commit

---
+++ suhail.py
@@ -0,0 +1,31 @@
+a="suhail"
+print(a)
+
+age=20
+print(age)
+
+num=14.5
+print(num)
+
+
+b=[1,2,3,4,5]
+print(b[-1])
+b.append(6)
+print(b)
+b.insert(0,0)
+print(b)
+b.pop(2)
+print(b)
+b.pop(0)
+print(b)
+c=("apple,banana,grapes")
+print(c)
+d=list(c)
+d.append("orange")
+print(c)
+
+Age=25
+if Age>=18:
+    print("you can vote.")
+else:  
+    print("you cannot vote.")
