c0406a607d1548c8a07194698cc8fec1e3e7bc0a
suhail <suhailalikka110@gmail.com>
2026-04-17 15:00:08 +0530

    python

---
+++ listofstudents.py
@@ -0,0 +1,3 @@
+students = ["Aman", "Suhail", "arjun", "Rahul", "Anand"]
+for name in students:
+    print("Hello", name + "! Welcome to the class ")
---
+++ oddorevenusingfunctio.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+check_even_odd(number)
